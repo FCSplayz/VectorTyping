8ff6fa330a847b5916b3c5e138ae90f8c4ca0c21
FCSplayz <40332447+FCSplayz@users.noreply.github.com>
2023-05-10 20:09:26 -0700

    Update Vector4Int.cs

--- Vector4Int.cs
+++ Vector4Int.cs
@@ -1,5 +1,5 @@
 // This code is licensed under the VectorTyping Library License.
-//
+///
 ///  | MIT License
 ///  | 
 ///  | Copyright (c) 2023-2024 FCSplayz and Unity Technologies
@@ -7,7 +7,7 @@
 ///  | Permission is hereby granted, free of charge, to any person obtaining a copy
 ///  | of this library and associated documentation files (the "Library"), to deal
 ///  | in the Library or any derivative works thereof with the following conditions
-//
+///
 // Refer to the accompanying 'LICENSE.md' file for more information.
 
 #region Assembly VectorTyping, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
